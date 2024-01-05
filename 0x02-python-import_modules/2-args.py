fa7687a0186b5f50390ae62c55b0b75d3fd16b90
bruzwyn <bruce.koech.7@gmail.com>
2024-01-05 10:25:15 -0800

    my commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 import sys
-if __name__ == "__main__":
-    num_args = len(sys.argv) - 1
 
-    print("{} argument{}{}".format(
-        num_args,
-        's' if num_args != 1 else '',
-        ':' if num_args > 0 else '.'
-        ))
-    for i in range(num_args):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+
+def main():
+    argv_len = len(sys.argv) - 1
+    print('{:d}'.format(argv_len),
+          ('argument' if argv_len == 1 else 'arguments') +
+          ('.' if argv_len == 0 else ':'))
+    for i in range(1, argv_len + 1):
+        print('{}: {}'.format(i, sys.argv[i]))
+
+
+if __name__ == "__main__":
+    main()
